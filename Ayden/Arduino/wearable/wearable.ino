8815f988c7cd8f3b688e15d0931c73ee6af6d8f9
Ayden <alm624@uregina.ca>
2023-02-03 11:15:03 -0600

    Worked on more power-saving code for Arduino

--- Ayden/Arduino/wearable/wearable.ino
+++ Ayden/Arduino/wearable/wearable.ino
@@ -38,14 +38,16 @@ DFRobot_BloodOxygen_S_HardWareUart MAX30102(&Serial1, 9600);
 #endif
 char spo2[] = "\0\0\0\0\0\0\0";
 char pulse[] = "\0\0\0";
+char invalid_data[] = "000 000";
 RF24 radio(9,10);
 uint8_t address[] = {0xE6,0xE6,0xE6,0xE6,0xE6};
 uint8_t clk_divider = 1;
+uint8_t count = 0;
 void setup()
 {
-  clk_divider = 4;
-  CLKPR = 0x80;
-  CLKPR = 0x02;
+//  clk_divider = 2;
+//  CLKPR = 0x80;
+//  CLKPR = 0x01;
   radio.begin();
   printf_begin();
   radio.setAddressWidth(5);
@@ -58,7 +60,7 @@ void setup()
   radio.setDataRate(RF24_250KBPS);
   radio.disableCRC();
   radio.stopListening();
-  Serial.begin(115200);
+  Serial.begin(115200*clk_divider);
   while (false == MAX30102.begin())
   {
     Serial.println("init fail!");
@@ -75,8 +77,13 @@ void loop()
   MAX30102.getHeartbeatSPO2();
   while((MAX30102._sHeartbeatSPO2.SPO2 == -1) || (MAX30102._sHeartbeatSPO2.Heartbeat == -1))
   {
-    delay(64/clk_divider);
+    delay(4000/clk_divider);
     MAX30102.getHeartbeatSPO2();
+    count++;
+    if(count == 3)
+    {
+      break;
+    }
   }
 //  Serial.print("SPO2 is : ");
 //  Serial.print(MAX30102._sHeartbeatSPO2.SPO2);
@@ -84,28 +91,45 @@ void loop()
 //  Serial.print("heart rate is : ");
 //  Serial.print(MAX30102._sHeartbeatSPO2.Heartbeat);
 //  Serial.println("Times/min");
+//  delay(50);
 //  Serial.print("Temperature value of the board is : ");
 //  Serial.print(MAX30102.getTemperature_C());
 //  Serial.println(" ℃");
-  itoa(MAX30102._sHeartbeatSPO2.SPO2, spo2, 10);
-  itoa(MAX30102._sHeartbeatSPO2.Heartbeat, pulse, 10);
-  strcat(spo2," ");
-  strcat(spo2, pulse);
-//  printf(spo2);
-//  printf("\n");
-  radio.powerUp();
-  delay(5);
-  radio.write(&spo2,sizeof(spo2));
-  delay(5);
-  radio.powerDown();
-  for(uint8_t i = 0; i < 7; i++)
+  if(count == 3)
   {
-    spo2[i] = '\0';
+    count = 0;
+    radio.powerUp();
+    delay(5);
+    radio.write(&invalid_data,sizeof(invalid_data));
+    delay(5);
+    radio.powerDown();
+  }
+  else
+  {
+    count = 0;
+    itoa(MAX30102._sHeartbeatSPO2.SPO2, spo2, 10);
+    itoa(MAX30102._sHeartbeatSPO2.Heartbeat, pulse, 10);
+    strcat(spo2," ");
+    strcat(spo2, pulse);
+    //  printf(spo2);
+    //  printf("\n");
+    radio.powerUp();
+    delay(5);
+    radio.write(&spo2,sizeof(spo2));
+    delay(5);
+    radio.powerDown();
+    for(uint8_t i = 0; i < 7; i++)
+    {
+      spo2[i] = '\0';
+    }
   }
   MAX30102.sensorEndCollect();
 //  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, 
 //                 SPI_OFF, USART0_OFF, TWI_OFF);
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
+  for (uint8_t i=0; i<7; i++)
+  {
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  }
   delay(50);
   MAX30102.sensorStartCollect();
   delay(4000);
