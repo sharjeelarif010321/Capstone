5c03c5b3e64f480a82b437c8de87449cdb2bbbfd
Ayden Mack <alm624@uregina.ca>
2023-03-09 19:30:47 -0600

    Utilize TPL5111 on pin 7 and increase reliability of wearable

--- Ayden/Arduino/wearable/wearable.ino
+++ Ayden/Arduino/wearable/wearable.ino
@@ -15,9 +15,10 @@
 #include "printf.h"
 #include <stdio.h>
 #include <LowPower.h>
+#include <SoftwareSerial.h>
 
-#define I2C_COMMUNICATION  //use I2C for communication, but use the serial port for communication if the line of codes were masked
-
+//#define I2C_COMMUNICATION  //use I2C for communication, but use the serial port for communication if the line of codes were masked
+#define ARDUINO_AVR_UNO
 #ifdef  I2C_COMMUNICATION
 #define I2C_ADDRESS    0x57
   DFRobot_BloodOxygen_S_I2C MAX30102(&Wire ,I2C_ADDRESS);
@@ -26,14 +27,14 @@
  *    board   |             MCU                | Leonardo/Mega2560/M0 |    UNO    | ESP8266 | ESP32 |  microbit  |
  *     VCC    |            3.3V/5V             |        VCC           |    VCC    |   VCC   |  VCC  |     X      |
  *     GND    |              GND               |        GND           |    GND    |   GND   |  GND  |     X      |
- *     RX     |              TX                |     Serial1 TX1      |     5     |   5/D6  |  D2   |     X      |
- *     TX     |              RX                |     Serial1 RX1      |     4     |   4/D7  |  D3   |     X      |
+ *     RX     |              TX                |     Serial1 TX1      |     3     |   5/D6  |  D2   |     X      |
+ *     TX     |              RX                |     Serial1 RX1      |     2     |   4/D7  |  D3   |     X      |
  * ---------------------------------------------------------------------------------------------------------------*/
 #if defined(ARDUINO_AVR_UNO) || defined(ESP8266)
-SoftwareSerial mySerial(4, 5);
+SoftwareSerial mySerial(2, 3);
 DFRobot_BloodOxygen_S_SoftWareUart MAX30102(&mySerial, 9600);
 #else
-DFRobot_BloodOxygen_S_HardWareUart MAX30102(&Serial1, 9600); 
+DFRobot_BloodOxygen_S_HardWareUart MAX30102(&Serial, 9600); 
 #endif
 #endif
 char spo2[] = "\0\0\0\0\0\0\0";
@@ -42,14 +43,16 @@ char invalid_data[] = "000 000";
 RF24 radio(9,10);
 uint8_t address[] = {0xE6,0xE6,0xE6,0xE6,0xE6};
 uint8_t clk_divider = 1;
+uint8_t spo2_flag = 0;
+uint8_t pulse_flag = 0;
 uint8_t count = 0;
 void setup()
 {
 //  clk_divider = 2;
 //  CLKPR = 0x80;
 //  CLKPR = 0x01;
-  pinMode(8, OUTPUT);
-  digitalWrite(8, LOW);
+  pinMode(7, OUTPUT);
+  digitalWrite(7, LOW);
   radio.begin();
   printf_begin();
   radio.setAddressWidth(5);
@@ -79,59 +82,97 @@ void loop()
   MAX30102.getHeartbeatSPO2();
   while((MAX30102._sHeartbeatSPO2.SPO2 == -1) || (MAX30102._sHeartbeatSPO2.Heartbeat == -1))
   {
+    if((MAX30102._sHeartbeatSPO2.SPO2 != -1) && (spo2_flag == 0))
+    {
+      itoa(MAX30102._sHeartbeatSPO2.SPO2, spo2, 10);
+      // Serial.println("got SpO2");
+      spo2_flag = 1;
+    }
+    if((MAX30102._sHeartbeatSPO2.Heartbeat != -1) && (pulse_flag == 0))
+    {
+      itoa(MAX30102._sHeartbeatSPO2.Heartbeat, pulse, 10);
+      // Serial.println("got pulse");
+      pulse_flag = 1;
+    }
     delay(4000/clk_divider);
     MAX30102.getHeartbeatSPO2();
+    // Serial.print("SPO2 is : ");
+    // Serial.print(MAX30102._sHeartbeatSPO2.SPO2);
+    // Serial.println("%");
+    // Serial.print("heart rate is : ");
+    // Serial.print(MAX30102._sHeartbeatSPO2.Heartbeat);
+    // Serial.println("Times/min");
+    // delay(50);
+    // Serial.print("Temperature value of the board is : ");
+    // Serial.print(MAX30102.getTemperature_C());
+    // Serial.println(" ℃");
     count++;
-    if(count == 3)
+    if((count == 7) || ((spo2_flag == 1) && (pulse_flag == 1)))
     {
+      if (((spo2_flag == 1) && (pulse_flag == 1)))
+      {
+        count--;
+      }
       break;
     }
   }
-//  Serial.print("SPO2 is : ");
-//  Serial.print(MAX30102._sHeartbeatSPO2.SPO2);
-//  Serial.println("%");
-//  Serial.print("heart rate is : ");
-//  Serial.print(MAX30102._sHeartbeatSPO2.Heartbeat);
-//  Serial.println("Times/min");
-//  delay(50);
-//  Serial.print("Temperature value of the board is : ");
-//  Serial.print(MAX30102.getTemperature_C());
-//  Serial.println(" ℃");
-  if(count == 3)
+  if(count == 7)
   {
     count = 0;
+    // Serial.println("FAIL");
     radio.powerUp();
     delay(5);
     radio.write(&invalid_data,sizeof(invalid_data));
-    delay(5);
-    radio.powerDown();
   }
   else
   {
     count = 0;
-    itoa(MAX30102._sHeartbeatSPO2.SPO2, spo2, 10);
-    itoa(MAX30102._sHeartbeatSPO2.Heartbeat, pulse, 10);
+    if((spo2_flag == 0) || (pulse_flag == 0))
+    {
+      itoa(MAX30102._sHeartbeatSPO2.SPO2, spo2, 10);
+      itoa(MAX30102._sHeartbeatSPO2.Heartbeat, pulse, 10);
+    }
+    while (strlen(spo2) < 3)
+    {
+      char padded[7] = "0"; // create a temporary string with '0' at the beginning
+      strcat(padded, spo2); // append the original string to the temporary string
+      strcpy(spo2, padded);
+    }
+    while (strlen(pulse) < 3)
+    {
+      char padded[3] = "0"; // create a temporary string with '0' at the beginning
+      strcat(padded, pulse); // append the original string to the temporary string
+      strcpy(pulse, padded);
+    }
     strcat(spo2," ");
     strcat(spo2, pulse);
+    // Serial.println("SUCCESS");
+    // Serial.println(spo2);
     //  printf(spo2);
     //  printf("\n");
     radio.powerUp();
     delay(5);
     radio.write(&spo2,sizeof(spo2));
-    delay(5);
-    radio.powerDown();
-    for(uint8_t i = 0; i < 7; i++)
-    {
-      spo2[i] = '\0';
-    }
+  }
+  delay(5);
+  radio.powerDown();
+  spo2_flag = 0;
+  pulse_flag = 0;
+  for(uint8_t i = 0; i < 7; i++)
+  {
+    spo2[i] = '\0';
+  }
+  for(uint8_t i = 0; i < 3; i++)
+  {
+    pulse[i] = '\0';
   }
   MAX30102.sensorEndCollect();
-  digitalWrite(8, HIGH); //Signal the TPL5111 to power down
+  digitalWrite(7, HIGH); //Signal the TPL5111 to power down
   // for (uint8_t i=0; i<7; i++)
   // {
   //   LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
   // }
-  // delay(50);
+  // delay(1000);
   // MAX30102.sensorStartCollect();
   // delay(4000);
 }
