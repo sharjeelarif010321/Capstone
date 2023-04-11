2e8182e41e108edf9579b6aa861d22c72e2c133d
sharjeelarif010321 <sharjeelarif010321@gmail.com>
2023-04-11 09:04:00 -0600

    Update splitDataSet.py with revision history

--- Sharjeel/trainingModelScripts/splitDataSet.py
+++ Sharjeel/trainingModelScripts/splitDataSet.py
@@ -1,78 +1,58 @@
+# This code creates training, validation, and test subsets of image data from a given dataset directory using the specified proportions. It creates the directories for each subset if they do not already exist, and defines the class names and size of the subsets
+
+###################################
+# Author: Sharjeel Arif
+# SID: 200388331
+# 08th March 2023
+###################################
+
+# REVISION HISTORY
+
+# 08 MAR 23: Created file to split the data into train, test, validation direcotry. 
+
+# 23 Mar 23: Changed percentage of images to allocate to train = 80%, test = 10%, validation = 10% from train = 60%, test = 20%, validation = 20%
+
 import os
 import shutil
-from sklearn.model_selection import train_test_split
+import random
 
-# Define the path to the directory containing the labeled images
-data_dir = '/Users/sharjeelarif/Desktop/CapstoneTrainingModel'
+dataset_dir = '/Users/sharjeelarif/Desktop/CapstoneTrainingModel/dataset'
 
 # Define the paths to the training, validation, and test directories
-train_dir = 'data/train'
-val_dir = 'data/val'
-test_dir = 'data/test'
+train_dir = '/Users/sharjeelarif/Desktop/CapstoneTrainingModel/dataset/train'
+val_dir = '/Users/sharjeelarif/Desktop/CapstoneTrainingModel/dataset/val'
+test_dir = '/Users/sharjeelarif/Desktop/CapstoneTrainingModel/dataset/test'
 
-# Define the test size and validation split size
-test_size = 0.2
-val_split_size = 0.25
+# Create directories for training, validation, and testing subsets
+os.makedirs(train_dir, exist_ok=True)
+os.makedirs(val_dir, exist_ok=True)
+os.makedirs(test_dir, exist_ok=True)
 
-# Get a list of all the image filenames and labels
-back_filenames = os.listdir(os.path.join(data_dir, 'lyingOnBack'))
-belly_filenames = os.listdir(os.path.join(data_dir, 'lyingOnBelly'))
-side_filenames = os.listdir(os.path.join(data_dir, 'lyingOnSide'))
-sitting_filenames = os.listdir(os.path.join(data_dir, 'sittingDown'))
+# Define the class names
+class_names = ['lyingOnBack', 'lyingOnBelly', 'lyingOnSide', 'sittingDown']
 
-back_labels = ['back'] * len(back_filenames)
-belly_labels = ['belly'] * len(belly_filenames)
-side_labels = ['side'] * len(side_filenames)
-sitting_labels = ['sitting'] * len(sitting_filenames)
+# Define the size of the subsets
+train_size = 0.8
+val_size = 0.1
+test_size = 0.1
 
-filenames = back_filenames + belly_filenames + side_filenames + sitting_filenames
-labels = back_labels + belly_labels + side_labels + sitting_labels
-
-# Split the dataset into training and test sets
-train_filenames, test_filenames, train_labels, test_labels = train_test_split(filenames, labels, test_size=test_size, stratify=labels)
-
-# Split the training set into training and validation sets
-train_filenames, val_filenames, train_labels, val_labels = train_test_split(train_filenames, train_labels, test_size=val_split_size, stratify=train_labels)
-
-# Create the subdirectories for each class in the training, validation, and test directories
-for label in set(train_labels):
-    os.makedirs(os.path.join(train_dir, label), exist_ok=True)
-    os.makedirs(os.path.join(val_dir, label), exist_ok=True)
-    os.makedirs(os.path.join(test_dir, label), exist_ok=True)
-
-# Copy the images to the corresponding subdirectories in the training, validation, and test directories
-for filename, label in zip(train_filenames, train_labels):
-    if filename in back_filenames:
-        src = os.path.join(data_dir, 'lyingOnBack', filename)
-    elif filename in belly_filenames:
-        src = os.path.join(data_dir, 'lyingOnBelly', filename)
-    elif filename in side_filenames:
-        src = os.path.join(data_dir, 'lyingOnSide', filename)
-    else:
-        src = os.path.join(data_dir, 'sittingDown', filename)
-    dst = os.path.join(train_dir, label, filename)
-    shutil.copy(src, dst)
+# Loop over each class and split the images into the subsets
+for class_name in class_names:
+    class_dir = os.path.join(dataset_dir, class_name)
+    file_names = os.listdir(class_dir)
+    random.shuffle(file_names)
+    num_files = len(file_names)
+    train_end = int(num_files * train_size)
+    val_end = int(num_files * (train_size + val_size))
     
-for filename, label in zip(val_filenames, val_labels):
-    if filename in back_filenames:
-        src = os.path.join(data_dir, 'lyingOnBack', filename)
-    elif filename in belly_filenames:
-        src = os.path.join(data_dir, 'lyingOnBelly', filename)
-    elif filename in side_filenames:
-        src = os.path.join(data_dir, 'lyingOnSide', filename)
-    else:
-        src = os.path.join(data_dir, 'sittingDown', filename)
-    dst = os.path.join(val_dir, label, filename)
-    shutil.copy(src, dst)
-    
-for filename, label in zip(test_filenames, test_labels):
-    if filename in back_filenames:
-        src = os.path.join(data_dir, 'lyingOnBack', filename)
-    elif filename in belly_filenames:
-        src = os.path.join(data_dir, 'lyingOnBelly', filename)
-    elif filename in side_filenames:
-        src = os.path.join(data_dir, 'lyingOnSide', filename)
-    else:
-        src = os.path.join(data_dir, 'sittingDown', filename)
-    dst = os.path.join(test_dir, label, filename)
-    shutil.copy(src, dst)
+    # Copy images to the appropriate subset directories
+    for i, file_name in enumerate(file_names):
+        src = os.path.join(class_dir, file_name)
+        if i < train_end:
+            dst = os.path.join(train_dir, class_name, file_name)
+        elif i < val_end:
+            dst = os.path.join(val_dir, class_name, file_name)
+        else:
+            dst = os.path.join(test_dir, class_name, file_name)
+        os.makedirs(os.path.dirname(dst), exist_ok=True)
+        shutil.copyfile(src, dst)
